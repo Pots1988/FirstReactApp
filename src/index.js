c57702daf90ebacac7c76fc6f95e1098a1fbc47d
pots1988bear@gmail.com <pots1988bear@gmail.com>
2020-01-01 21:29:29 +0200

    Added components

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const el = (
-  <div>
-    <h1>My Todo List</h1>
-    <input placeholder="search" />
-    <ul>
-      <li>Learn React</li>
-      <li>Build Awesome App</li>
-    </ul>
-  </div>
-);
+import AppHeader from './components/app-header';
+import SearchPanel from './components/search-panel';
+import TodoList from './components/todo-list';
 
-console.log(el);
+const App = () => {
 
-ReactDOM.render(el, document.getElementById('root'));
+  const todoData = [
+    { label: 'Drink Coffee', important: false, id: 1 },
+    { label: 'Make Awesome App', important: true, id: 2 },
+    { label: 'Have a lunch', important: false, id: 3 }
+  ];
+
+  return (
+    <div>
+      <AppHeader />
+      <SearchPanel />
+      <TodoList todos={ todoData } />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
