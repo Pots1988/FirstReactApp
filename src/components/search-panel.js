cd4d37d2ce2b4f7062185e59dce7083d9feb98b8
Maxim Fariga <fm@itsvit.org>
2020-01-02 10:29:37 +0200

    Added styles

--- src/components/search-panel.js
+++ src/components/search-panel.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import ItemStatusFilter from './item-status-filter';
+import './search-panel.css';
 
 const SearchPanel = () => {
-  return <input placeholder="search" />;
+  return (
+      <div>
+        <input placeholder="search" />
+        <ItemStatusFilter />
+      </div>
+  );
 };
 
 export default SearchPanel;
